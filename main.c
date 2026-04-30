b50984fdb9429d54797cf7a36388ceb17f30fb26
Igor Lucas <igorlucasdeoficiall@gmail.com>
2026-04-30 15:10:19 -0300

    update main.c remove comments

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
-#include <stdio.h> // Biblioteca padrão de entrada e saída
-
-// Declaração de funções/variáveis globais (opcional)
+#include <stdio.h>
 
 int main()
 {
-    // Corpo do programa: instruções aqui
+
     int a, b, c, validation = 0;
 
     printf("What is the length of a side of triangle A?");
@@ -16,24 +14,28 @@ int main()
     printf("What is the length of a side of triangle C?");
     scanf("%d", &c);
 
-    //Se (lado1 < lado2 + lado3) E (lado2 < lado1 + lado3) E (lado3 < lado1 + lado2) Então
     validation = (a < b + c) && (b < a + c) && (c < a + b);
 
     if (validation)
     {
-         //lado1 = lado2) E (lado2 = lado3) Então
+
         if ((a == b) && (b == c))
         {
             printf("Equilateral");
-        }else if((a == b) || (a == c) || (b == c)){
+        }
+        else if ((a == b) || (a == c) || (b == c))
+        {
             printf("Isosceles");
-        }else{
+        }
+        else
+        {
             printf("Scalene");
         }
-    }else{
+    }
+    else
+    {
         printf("Invalid triangle");
     }
-    
 
-    return 0; // Indica que o programa terminou com sucesso
+    return 0; 
 }
